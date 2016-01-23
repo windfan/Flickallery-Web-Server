16fd51249a487a4391d69f5a225b93795601ca1d
Tzu-Hao Huang <ryu1031@gmail.com>
2016-01-23 16:47:04 -0500

    Add mocha to test 200 status code with supertest

--- Test/clientSideTest.js
+++ Test/clientSideTest.js
@@ -3,10 +3,12 @@
 var request = require('supertest');
 var app = require('./../app');
 
-request(app)
-  .get('/')
-  .expect(200)
-  .end(function(error, response){
-    if(error) throw error;
-    console.log('Test done successfully!');
+describe('GET /', function(){
+
+  it('Returns a 200 status code', function(done){
+    request(app)
+      .get('/')
+      .expect(200, done);
   });
+
+});

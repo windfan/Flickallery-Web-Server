9f5e5ea531d702eb23e9ecaa422825f2c925ab70
Tzu-Hao Huang <ryu1031@gmail.com>
2016-01-23 21:36:35 -0500

    Test for socket.io for connection with fake data

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.get('/', function(request, response){
   response.sendfile('public/index.html');
 });
 
-/* Get /photos path */
-app.get('/photos', function(request, response){
-  let photos = {
-    "1" : 'https://c1.staticflickr.com/1/600/23577541545_007c408d75_c.jpg',
-    "2" : 'https://c2.staticflickr.com/6/5649/22679451641_e9db431625_c.jpg',
-    "3" : 'https://c2.staticflickr.com/6/5667/22112428156_41441e6de5_c.jpg'
-  };
-  response.json(photos);
-});
+// /* Get /photos path */
+// app.get('/photos', function(request, response){
+//   let photos = {
+//     "1" : 'https://c1.staticflickr.com/1/600/23577541545_007c408d75_c.jpg',
+//     "2" : 'https://c2.staticflickr.com/6/5649/22679451641_e9db431625_c.jpg',
+//     "3" : 'https://c2.staticflickr.com/6/5667/22112428156_41441e6de5_c.jpg'
+//   };
+//   response.json(photos);
+// });
 
 module.exports = app;
---
+++ models/photos.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var host = process.env.PHOTOS_HOST || "http://localhost:8000/photos";
+var request = require('request');
